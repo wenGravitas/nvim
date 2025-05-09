c16770c6a4c059c32a01cfc532dca3e58c2f5b59
wenGravitas <dev@iqterminal.com>
2025-05-09 11:56:40 +0300

    new tab config for autocomplete/copilot

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -9,12 +9,42 @@ return {
   },
 
   opts = {
-    keymap = { preset = "super-tab" },
+    keymap = {
+      preset = nil, -- disable built-in presets
+      custom = {
+        ["<Tab>"] = function()
+          local copilot = require("copilot.suggestion")
+          local luasnip = require("luasnip")
+
+          if copilot.is_visible() then
+            copilot.accept()
+          elseif vim.fn.pumvisible() == 1 then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, false, true), "n", true)
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", true)
+          end
+        end,
+
+        ["<S-Tab>"] = function()
+          local luasnip = require("luasnip")
+          if vim.fn.pumvisible() == 1 then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, false, true), "n", true)
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true), "n", true)
+          end
+        end,
+      },
+    },
+
     appearance = {
       nerd_font_variant = "mono",
     },
+
     completion = {
-      --  list = { selection = "auto_insert" },
       menu = {
         draw = {
           columns = { { "kind_icon" }, { "label", gap = 1 } },
@@ -34,6 +64,7 @@ return {
         auto_show = false,
       },
     },
+
     sources = {
       default = { "avante", "lsp", "path", "snippets", "buffer" },
       providers = {
@@ -49,12 +80,14 @@ return {
 
     fuzzy = { implementation = "prefer_rust_with_warning" },
   },
+
   opts_extend = { "sources.default" },
+
   config = function(_, opts)
     require("colorful-menu").setup()
     require("blink.cmp").setup(opts)
 
-    -- Muted FloatBorder
+    -- Muted FloatBorder for popup windows
     vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#5c5c5c", bg = "#1e1e1e" })
   end,
 }
